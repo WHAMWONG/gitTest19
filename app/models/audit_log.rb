f5036c7dfa6930ae51bcf27977130afdb0547d9a
Darya (Jitera) <developers@jitera.com>
2024-01-17 02:23:14 +0000

    feat[service]: Log deletion action

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,8 +1,13 @@
+
 class AuditLog < ApplicationRecord
   belongs_to :user
 
   # validations
-
+  validates :user_id, presence: true, numericality: { only_integer: true }
+  validates :action, presence: true
+  validates :entity_type, presence: true
+  validates :entity_id, presence: true, numericality: { only_integer: true }
+  validates :timestamp, presence: true
   # end for validations
 
   class << self
